661990254ee5cd0c579f07d943431a7e5d441d6a
Debkanta Mitra <debkanta.mitra@leapfinance.com>
2025-04-19 03:16:58 +0530

    feat: autocomplete with cache, debounce

--- src/problems/Autocomplete/index.jsx
+++ src/problems/Autocomplete/index.jsx
@@ -11,17 +11,33 @@ const Autocomplete = () => {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
   const [showResults, setShowResults] = useState(false);
+  const [cache, setCache] = useState({});
+  const [highlightedIndex, setHighlightedIndex] = useState();
 
   const getRecipesByQuery = async () => {
+    const trimmedQuery = query.trimEnd();
+
+    if (cache[trimmedQuery]) {
+      console.log("Returned from cache for: " + trimmedQuery);
+      setResults(cache[trimmedQuery]);
+
+      return;
+    }
+
     try {
       const response = await fetch(
-        `https://dummyjson.com/recipes/search?q=${query}`
+        `https://dummyjson.com/recipes/search?q=${trimmedQuery}`
       );
 
       if (response.status === 200) {
         const json = await response.json();
 
+        // setting the cache, we could use
+        // localStorage for persistant if needed
+        setCache((prev) => ({ ...prev, [trimmedQuery]: json.recipes }));
         setResults(json.recipes);
+
+        console.log("Returned from API for: " + trimmedQuery);
       }
     } catch (e) {
       console.error(e);
@@ -29,12 +45,48 @@ const Autocomplete = () => {
   };
 
   useEffect(() => {
+    if (query.length < 1) return;
+    // example of debounce with vanilla js
+
+    // function debounce(callback, delay) {
+    //      let timer;
+    //
+    //      function (...args) {
+    //         clearTimeout(timer);
+    //
+    //         timer = setTimeout(() => callback(...args), delay)
+    //      }
+    //}
     const timer = setTimeout(getRecipesByQuery, 300);
 
     return () => {
+      // usual misconception is that the clean up happens only when component de mounts,
+      // but it happens whenever the dependency change and the effect re runs
+
       clearTimeout(timer);
     };
   }, [query]);
+
+  // useEffect(() => {
+  //   document.addEventListener("keydown", (e) => {
+  //     const key = e.key;
+
+  //     if (key === "ArrowUp" && highlightedIndex > 0) {
+  //       setHighlightedIndex(highlightedIndex - 1);
+  //     } else if (key === "ArrowDown") {
+  //       // console.log(highlightedIndex, results.length);
+  //       if (typeof highlightedIndex === "undefined") {
+  //         setHighlightedIndex(0);
+  //       }
+
+  //       if (highlightedIndex >= 0) {
+  //         setHighlightedIndex(highlightedIndex + 1);
+  //       }
+  //     }
+  //   });
+  // }, []);
+
+  // console.log(highlightedIndex, "@index");
 
   return (
     <div className={styles.container}>
@@ -54,17 +106,20 @@ const Autocomplete = () => {
       />
 
       {showResults && (
-        <div className="result__container">
+        <div className={styles.result__container}>
           {results.length > 0 ? (
             results.map((item, index) => {
               return (
-                <span key={index} className="result">
+                <span
+                  key={index}
+                  className={`${styles.result} ${index === highlightedIndex && styles.result__active}`}
+                >
                   {item.name}
                 </span>
               );
             })
           ) : (
-            <span className="result" style={{ textAlign: "center" }}>
+            <span className={styles.result} style={{ textAlign: "center" }}>
               No Results
             </span>
           )}
